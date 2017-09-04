b3699ba541fd42d37bb9d9e0847591ab075a7949
shruti_R <shruti@xelpmoc.in>
2017-09-04 12:35:45 +0530

    list container

---
+++ src/components/Listitem.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import "./css/Listitem.css";
+
+export default class Listitem extends Component {
+  render() {
+    return <div className="Listitem" />;
+  }
+}
