86c552989596433b93750df75d0eb67ab457094d
mohdniyazkhan <niyazkhan110@gmail.com>
2017-09-06 17:34:52 +0530

    update from niyaz

--- src/App - Copy.js
+++ src/App - Copy.js
@@ -1,24 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import Header from "./components/Header"
-import ListItem from "./components/ListItem"
 import ListHeader from "./components/ListHeader"
-import Input from "./components/Input"
+
+import HeadItems from "./components/ListHeader"
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-    
+     
       <div className="App">
-       <Header/>
+        <Header/>
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>started Learning React!</h2>
         </div>
-         <ListHeader />
-        <ListItem/>
-        <Input/>    
+        <ListHeader />
       </div>
     );
   }
